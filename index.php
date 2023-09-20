e3a60563f222df6c982026218063c250feaa4e66
sureniafrilliyah <144641206+sureniafrilliyah@users.noreply.github.com>
2023-09-20 16:36:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,28 @@
       <?php
       if(isset($_GET['pesan'])){
           if($_GET['pesan']=="gagal"){
-             echo "<div class='alert'username dan password tidak sesuai !</div>";
+             echo "<div class='alert'>username dan password tidak sesuai !</div>";
           }
       }
 ?>
 <div class="kotak_login">
 <p class="tulisan_login"></p>
-    bxhxhxjfufififjfjf
 
-<form
+<form action="cek_login.php" method="post">
+    <label>Username</label>
+    <input type="text" name="username" class="from_login" placeholder="username .." required="required">
+
+    <label>Password</label>
+    <input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+
+<input type="submit" class="tombol_login" value="LOGIN">
+
+    <br/>
+    <br/>
+    <center>
+        <a class="link" href="https://www.malasngoding.com">kembali</a>
+    </center>
+</from>
+</div>
+</body>
+</html>
