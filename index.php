2b8e67a6b561dd96bce039c546f575ba7469b70a
sureniafrilliyah <144641206+sureniafrilliyah@users.noreply.github.com>
2023-09-20 10:40:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Membuat login multi user level dengan PHP dan MySQLi - www.malasngoding.com</title>
+    <link rel ="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+     <h1>membuat login multi user level dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
+
+      <?php
+      if(isset($_GET['pesan'])){
+          if($_GET['pesan']=="gagal"){
+             echo "<div class='alert'username dan password tidak sesuai !</div>";
+          }
+      }
+?>
+<div class="kotak_login">
+<p class="tulisan_login"></p>
+
+<form
