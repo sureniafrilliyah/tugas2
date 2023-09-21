feda3b272bb3084944d2af6de0370bdd6039bf06
sureniafrilliyah <144641206+sureniafrilliyah@users.noreply.github.com>
2023-09-21 16:39:22 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,2 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("localhost","root","","multi_user");
 
-ajabshsb
+// Chek connection
+if (mysqli_connect_errno()){
+    echo "koneksi database gagal : " . mysqli_connect_error();
+}
+
+?>
