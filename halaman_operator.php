39b68b2622807e715a010db5dc74773f8507009c
sureniafrilliyah <144641206+sureniafrilliyah@users.noreply.github.com>
2023-09-21 16:47:44 +0700

    Create halaman_operator.php

---
+++ halaman_operator.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman operator</title>
+</head>
+<body>
+    <?php 
+    session_start();
+
+    // cek apakah yang mengakses halaman ini sudah login
+    if($_SESSION['level']==""){
+         header("location:index.php?pesan=gagal");
+}
+
+?>
+<hl>Halaman pengurus</hl>
+
+<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b>
+<?php echo $_['level']; ?></b>.</p>
+<a href="logout.php">LOGOUT</a>
+
+<br/>
+<br/>
+
+<a>Membuat Login Multi Level Dengan PHP</a>
+</body>
+</html>
+
