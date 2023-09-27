093bf6e3a9ad4eec0520b3cd27d86677b378cda3
sureniafrilliyah <144641206+sureniafrilliyah@users.noreply.github.com>
2023-09-27 16:16:08 +0700

    Update halaman_admin.php

--- halaman_admin.php
+++ halaman_admin.php
@@ -15,7 +15,7 @@
 ?>
 <h1>Halaman Admin</h1>
 
-<p>Halo<b><?php echo $_SESSION['username'];?></b> Anda telah lohin sebagai <b><?php echo $_SESSION['level'];?></b>.</p>
+<p>Halo<b><?php echo $_SESSION['username']; ?></b> Anda telah lohin sebagai <b><?php echo $_SESSION['level'];?></b>.</p>
 <a href="logout.php">LOGOUT</a>
 
 <br/>
